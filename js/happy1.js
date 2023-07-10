8b741e8a48197ffdd12fdd12f146935a80c88b33
codefoxs <59310678+codefoxs@users.noreply.github.com>
2023-07-10 22:36:44 +0800

    Add files via upload

---
+++ js/happy1.js
@@ -0,0 +1,16 @@
+$(function () {
+    setTimeout(function () {
+        $('.button-style1').animate({
+            opacity:"1",
+            top:"70%"
+        },0);
+        $('.button-style2').animate({
+            opacity:"1",
+            top:"85%"
+        },0);
+    },0);
+	// $(window).click(function(){
+	// 	$('audio')[0].play();
+	// });
+	// window.audio=$('audio')[0].play();
+})
